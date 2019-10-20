538d64e19f54bc25090425116ab459f9a9922bbf
Steven Song <steven.song98@gmail.com>
2019-10-20 13:22:26 -0400

    create db if not exists

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./controllers/queries')
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./models');
+let models;
+const port = 3000;
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.json())
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-)
+);
 
 app.get('/', function (req, res) {
-  res.render('index')
-})
+  res.render('index');
+});
 
-app.get('/persons', db.getUsers)
+app.get('/users', (req, res) => {
+  models.User.findAll().then((users) => {
+    res.status(200).json(users);
+  });
+});
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+// start app only after database is created and models are synchronized
+db.init((_models) => {
+  models = _models;
+  models.sequelize.sync().then(() => {
+    app.listen(port, () => {
+      console.log(`App running on port ${port}.`);
+    });
+  });
+});
