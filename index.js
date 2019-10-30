0e26b4aedab80e9fd129641b98a7cfef8a63174d
Steven Song <steven.song98@gmail.com>
2019-10-30 13:41:15 -0400

    fix linting

--- index.js
+++ index.js
@@ -23,38 +23,32 @@ db.init((models) => {
       console.log(`App running on port ${port}.`);
 
       // test create user
-      db.createUser('bobbyS', 'bob', 'smith', 'email@email.com')
-        .then((newUser) => {
+      db.createUser('bobbyS', 'bob', 'smith', 'email@email.com').then((newUser) => {
         console.log("CREATED NEW USER");
         console.log(newUser);
 
         // test get user
-        db.getUser('bobbyS')
-          .then((user) => {
+        db.getUser('bobbyS').then((user) => {
           console.log("FOUND USER: bobbyS");
           console.log(user);
 
           // test change username
-          db.changeUsername('bobbyS', 'robertS')
-            .then((user) => {
+          db.changeUsername('bobbyS', 'robertS').then((user) => {
             console.log("USERNAME CHANGE");
             console.log(user);
 
             // test other user attribute modifiers
-            db.changeEmail('robertS', 'real@email.com')
-              .then((user) => {
+            db.changeEmail('robertS', 'real@email.com').then((user) => {
               console.log("EMAIL CHANGE");
               console.log(user);
             });
 
-            db.changeFirstName('robertS', 'robert')
-              .then((user) => {
+            db.changeFirstName('robertS', 'robert').then((user) => {
               console.log("FIRST NAME CHANGE");
               console.log(user);
             });
 
-            db.changeLastName('robertS', 'smith-jenkins')
-              .then((user) => {
+            db.changeLastName('robertS', 'smith-jenkins').then((user) => {
               console.log("LAST NAME CHANGE");
               console.log(user);
             });
