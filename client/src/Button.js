3cf1c6b2321842e54406844166817fc73ee30f61
Peter Lam <Peter.Lam@tufts.edu>
2019-11-11 15:58:17 -0500

    Updated with registration form and fake authentication; cleaned up CSS on registration page

--- client/src/Button.js
+++ client/src/Button.js
@@ -4,9 +4,16 @@ import './Button.css'
 class Button extends React.Component {
     render() {
         return (
-            <p className="Button">
-                <a href={this.props.url}>{this.props.name}</a>
-            </p>
+            // <button className="Button" onClick={() => {
+            //     this.props.to ? 
+            //     (location.href = this.props.to) : 
+            //     this.props.onClick
+            // }}>
+            //     {this.props.name}
+            // </button>
+            // <p className="Button">
+                <a className="Button" onClick={this.props.onClick} href={this.props.url}>{this.props.name}</a>
+            // </p>
         );
     }
 }
