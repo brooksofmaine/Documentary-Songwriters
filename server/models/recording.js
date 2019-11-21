0eabff8231df91f23a944eea25655de877b3d877
Emily Liu <emilycliu1@gmail.com>
2019-11-20 19:54:13 -0500

    recording start is the primary key

--- server/models/recording.js
+++ server/models/recording.js
@@ -1,6 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   let Recording = sequelize.define('Recording', {
-    start: DataTypes.DATE,
+    start: {
+      type: DataTypes.DATE,
+      primaryKey: true
+    },
     end: DataTypes.DATE,
     numPitches: DataTypes.INTEGER,
     instrument: DataTypes.STRING,
