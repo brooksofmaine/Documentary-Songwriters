4b7f81364e68952ba202246a972d5b919a669fc2
Peter Lam <Peter.Lam@tufts.edu>
2020-02-29 11:30:49 -0500

    Started adding routing functions

--- client/src/groups-page/NewGroup.js
+++ client/src/groups-page/NewGroup.js
@@ -6,6 +6,9 @@ import NewMember from './NewMember';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
+import {server_add} from "../api-helper/config";
+
+
 class NewGroup extends React.Component {
     constructor() {
         super();
@@ -19,6 +22,31 @@ class NewGroup extends React.Component {
 
         this.handleChange  = this.handleChange.bind(this);
         this.clickToDelete = this.clickToDelete.bind(this);
+        this.createGroup = this.createGroup.bind(this)
+    }
+
+    async createGroup() {
+        const groupInfo = {
+            groupName: this.state.groupName,
+            description: "TODO: Add description to front-end groups.",
+            visible: this.state.publicity === 'public' ? true : false
+        }
+        const query_url = server_add + '/api/group/create'
+        const post_params = {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(groupInfo)
+        }
+        const response = await fetch(query_url, post_params);
+        const body = await response.json();
+        console.log("Response body: ", body)
+        if (response.status !== 200) {
+          throw Error(body.message) 
+        }
+        return body;
     }
 
     handleChange(event) {
@@ -69,66 +97,71 @@ class NewGroup extends React.Component {
                 <h2 className = "GroupHeading">Create a Group</h2>
                 <div className = "GroupBox">
                     <form>
-                        <div className = "InputWrapper">  
+                        <div className="InputWrapper">  
                             <input 
-                                type = "text" 
-                                name = "groupName"
-                                onChange = {this.handleChange}
-                                value = {this.state.groupName}
-                                placeholder = "Group Name"
-                                className = "GroupFormText"
+                                type="text" 
+                                name="groupName"
+                                onChange={this.handleChange}
+                                value={this.state.groupName}
+                                placeholder="Group Name"
+                                className="GroupFormText"
                             />
                         </div>
                         <br />
 
-                        <div className = "InputWrapper">
+                        <div className="InputWrapper">
                             <input 
-                                type = "text" 
-                                name = "currMember"
-                                onChange = {this.handleChange}
-                                value = {this.state.currMember}
-                                placeholder = "Search members to invite"
-                                className = "GroupFormText"
+                                type="text" 
+                                name="currMember"
+                                onChange={this.handleChange}
+                                value={this.state.currMember}
+                                placeholder="Search members to invite"
+                                className="GroupFormText"
                             />
-                            <span><FontAwesomeIcon icon={faSearch} className = "SearchIcon"/></span>
+                            <span><FontAwesomeIcon icon={faSearch} className="SearchIcon"/></span>
                         </div>
                         <br />
 
-                        <div className = "NewGroupMemberBox">
+                        <div className="NewGroupMemberBox">
                             {newMembers}
                         </div>
 
-                        <div className = "RadioBox">
-                            <label className = "RadioLabel" style = {privateStyle}>
+                        <div className="RadioBox">
+                            <label className="RadioLabel" style={privateStyle}>
                                 <input 
-                                    type = "radio" 
-                                    name = "publicity"
-                                    value = "private"
-                                    onChange = {this.handleChange}
-                                    className = "RadioButton"
+                                    type="radio" 
+                                    name="publicity"
+                                    value="private"
+                                    onChange={this.handleChange}
+                                    className="RadioButton"
                                 />
-                                <div className = "NoCheckButton"></div>
-                                <FontAwesomeIcon icon={faCheckCircle} className = "CheckButton"/>
+                                <div className="NoCheckButton"></div>
+                                <FontAwesomeIcon icon={faCheckCircle} className="CheckButton"/>
                                 Private
                             </label>
 
-                            <label className = "RadioLabel" style = {publicStyle}>
+                            <label className="RadioLabel" style={publicStyle}>
                                 <input 
-                                    type = "radio" 
-                                    name = "publicity"
-                                    value = "public"
-                                    onChange = {this.handleChange}
-                                    className = "RadioButton"
+                                    type="radio" 
+                                    name="publicity"
+                                    value="public"
+                                    onChange={this.handleChange}
+                                    className="RadioButton"
                                 />
-                                <div className = "NoCheckButton"></div>
-                                <FontAwesomeIcon icon={faCheckCircle} className = "CheckButton"/>
+                                <div className="NoCheckButton"></div>
+                                <FontAwesomeIcon icon={faCheckCircle} className="CheckButton"/>
                                 Public
                             </label>
                         </div>
                         <br />
 
-                        <div className = "ButtonBox">
-                            <button className = "GroupButton">Create Group</button>
+                        <div className="ButtonBox">
+                            <button 
+                                className="GroupButton" 
+                                onClick={() =>  {
+                                    this.createGroup();
+                                    this.props.history.push("/api/groups");
+                                }}>Create Group</button>
                         </div>
                     </form>
                 </div>
