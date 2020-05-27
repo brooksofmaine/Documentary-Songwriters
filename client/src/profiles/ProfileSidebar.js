c94b0c371dc4c0ad13fc3660cf1b060a763cac87
Peter Lam <Peter.Lam@tufts.edu>
2020-05-27 12:08:55 -0400

    Updated sidebar code
    Bug still exists with getting window location + highlighting correct page on refresh

--- client/src/profiles/ProfileSidebar.js
+++ client/src/profiles/ProfileSidebar.js
@@ -1,104 +1,134 @@
 import React, {useState, useEffect} from 'react';
+import {Link, useRouteMatch, useParams} from 'react-router-dom'
 import './ProfileSidebar.css';
 import UserFunc from '../api-helper/user.js';
 
 function ProfileSidebar() {
     // TODO: Centralize
 
-    const normalContainer = {backgroundColor: "#EEE", fontWeight: 200};
-    const hoverContainer = {backgroundColor: "#8EACCD", fontWeight: 700};
-    const selectedContainer = {backgroundColor: "#0A1658", fontWeight: 700};
-    const normalText = "NormalText";
-    const hoverText = "HoverText";
-    const selectedText = "SelectedText";
+    // const normalContainer = {backgroundColor: "#EEE", fontWeight: 200};
+    // const hoverContainer = {backgroundColor: "#8EACCD", fontWeight: 700};
+    // const selectedContainer = {backgroundColor: "#0A1658", fontWeight: 700};
+    // const normalText = "NormalText";
+    // const hoverText = "HoverText";
+    // const selectedText = "SelectedText";
     
     const [page, setPage] = useState(0);
     const [username, setUsername] = useState("");
 
-    const [hoveringProgress, setHoveringProgress] = useState(false);
-    const [hoveringProfile, setHoveringProfile] = useState(false);
-    const [hoveringSettings, setHoveringSettings] = useState(false);
+    // const [hoveringProgress, setHoveringProgress] = useState(false);
+    // const [hoveringProfile, setHoveringProfile] = useState(false);
+    // const [hoveringSettings, setHoveringSettings] = useState(false);
 
-    const [progressText, setProgressText] = useState(selectedText);
-    const [progressContainer, setProgressContainer] = useState(selectedContainer);
-    const [profileText, setProfileText] = useState(normalText);
-    const [profileContainer, setProfileContainer] = useState(normalContainer);
-    const [settingsText, setSettingsText] = useState(normalText);
-    const [settingsContainer, setSettingsContainer] = useState(normalContainer);
+    // const [progressText, setProgressText] = useState(selectedText);
+    // const [progressContainer, setProgressContainer] = useState(selectedContainer);
+    // const [profileText, setProfileText] = useState(normalText);
+    // const [profileContainer, setProfileContainer] = useState(normalContainer);
+    // const [settingsText, setSettingsText] = useState(normalText);
+    // const [settingsContainer, setSettingsContainer] = useState(normalContainer);
 
+
+    const [selectedPage, setSelectedPage] = useState(0);
+    const [userPath, setUserPath] = useState("")
     
+    const id = useParams()
+    console.log("ID:", id)
+    // setUserPath(id)
+
     useEffect(() => {
-        
-        getUsernameAndPage();
+        // let {id } = useParams()
+        // if (id != "") {
+        //     setUserPath(id)
+        // }
+
         
         async function getUsernameAndPage() {
             const username = await UserFunc.getCurrentUsername();
             console.log(username);
-            setUsername(username);
-
-            const url = window.location.href;
-            console.log(url);
-            if (url.includes(username)) {
-                setPage(1);
-            }
-            else if (url.includes("settings")) {
-                setPage(2);
-            }
-            else {
-                setPage(0);
-            }
-            console.log(page)
+            setUsername(username)
         }
+        getUsernameAndPage();
+        updatePages();
+
     }, [])
 
-    useEffect(() => {
-        if (page === 0) {
-            setProgressContainer(selectedContainer);
-            setProgressText(selectedText);
-        }
-        else if (hoveringProgress) {
-            setProgressContainer(hoverContainer);
-            setProgressText(hoverText);
-        }
-        else {
-            setProgressContainer(normalContainer);
-            setProgressText(normalText);
-        }
-    }, [hoveringProgress])
+    // Current bug: calls when username is empty and so it automatically shades to this
+    const getWindowLocation = () => {
+        const url = window.location.href
 
-    useEffect(() => {
-        if (page === 1) {
-            setProfileContainer(selectedContainer);
-            setProfileText(selectedText);
+        if (url.includes("settings")) {
+            return 2
+        } else if (url.includes(username)) {
+            return 1
+        } else {
+            return 0
         }
-        else if (hoveringProfile) {
-            setProfileContainer(hoverContainer);
-            setProfileText(hoverText);
-        }
-        else {
-            setProfileContainer(normalContainer);
-            setProfileText(normalText);
-        }
-    }, [hoveringProfile])
+    }
 
-    useEffect(() => {
-        if (page === 2) {
-            setSettingsContainer(selectedContainer);
-            setSettingsText(selectedText);
-        }
-        else if (hoveringSettings) {
-            setSettingsContainer(hoverContainer);
-            setSettingsText(hoverText);
-        }
-        else {
-            setSettingsContainer(normalContainer);
-            setSettingsText(normalText);
-        }
-    }, [hoveringSettings])
+    const updatePages = (pageNum = getWindowLocation()) => {
+        console.log("Called with pageNum", pageNum)
+        setSelectedPage(pageNum)
+        
+    }
+
+    // useEffect(() => {
+    //     if (page === 0) {
+    //         setProgressContainer(selectedContainer);
+    //         setProgressText(selectedText);
+    //     }
+    //     else if (hoveringProgress) {
+    //         setProgressContainer(hoverContainer);
+    //         setProgressText(hoverText);
+    //     }
+    //     else {
+    //         setProgressContainer(normalContainer);
+    //         setProgressText(normalText);
+    //     }
+    // }, [hoveringProgress])
+
+    // useEffect(() => {
+    //     if (page === 1) {
+    //         setProfileContainer(selectedContainer);
+    //         setProfileText(selectedText);
+    //     }
+    //     else if (hoveringProfile) {
+    //         setProfileContainer(hoverContainer);
+    //         setProfileText(hoverText);
+    //     }
+    //     else {
+    //         setProfileContainer(normalContainer);
+    //         setProfileText(normalText);
+    //     }
+    // }, [hoveringProfile])
+
+    // useEffect(() => {
+    //     if (page === 2) {
+    //         setSettingsContainer(selectedContainer);
+    //         setSettingsText(selectedText);
+    //     }
+    //     else if (hoveringSettings) {
+    //         setSettingsContainer(hoverContainer);
+    //         setSettingsText(hoverText);
+    //     }
+    //     else {
+    //         setSettingsContainer(normalContainer);
+    //         setSettingsText(normalText);
+    //     }
+    // }, [hoveringSettings])
 
     return(
         <div className="ProfileSidebar">
-            <a className={progressText} href="/api/profile">
+            <Link className={selectedPage === 0 ? "sidebar-link selected-link" : "sidebar-link"}
+                  to="/api/profile"
+                  onClick={() => {updatePages(0)}}>Progress</Link>
+            <Link className={selectedPage === 1 ? "sidebar-link selected-link" : "sidebar-link"} 
+                  to={"/api/profile/" + username}
+                  onClick={() => {updatePages(1)}}>Profile</Link>
+            <Link className={selectedPage === 2 ? "sidebar-link selected-link" : "sidebar-link"}
+                  to="/api/profile/settings"
+                  onClick={() => {updatePages(2)}}>Settings</Link>
+
+            {/* <a className={progressText} href="/api/profile">
                 <p onMouseEnter={() => setHoveringProgress(true)} 
                    onMouseLeave={() => setHoveringProgress(false)} 
                    className="SidebarLink"
@@ -121,7 +151,7 @@ function ProfileSidebar() {
                    style={settingsContainer}>
                     Settings
                 </p>
-            </a>
+            </a> */}
         </div>
     )
 }
