8d084262de93e35592d10b430b8a8a971d4683cb
rebecca-millerr <me@beccamiller.com>
2020-06-04 18:24:20 -0500

    everything works but peter's stuff is missing

--- client/src/profiles/capitalization 3.js
+++
@@ -1,7 +0,0 @@
-export const lowerFirstLetter = (word) => {
-    return word.charAt(0).toLowerCase() + word.slice(1);
-}
-
-export const capFirstLetter = (word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
