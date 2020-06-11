c36fcd65b8f67e06653c6ed358dd96e604394d4d
waymao <my.waymao@gmail.com>
2020-06-12 02:12:01 +0800

    conceal google login clientID and secret.

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   google:{
-    clientID: '493863925829-47v1dkb5oh641a8fgb9a971hu990sht7.apps.googleusercontent.com',
-    clientSecret: 'HVrEoLQvhRT7z-N7XjiGq01m'
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
   }
-}
+};
