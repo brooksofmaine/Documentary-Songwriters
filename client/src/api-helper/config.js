c0a590968f6ef43865e38f16dd808267a3b7995f
waymao <my.waymao@gmail.com>
2020-06-12 02:01:19 +0800

    allow auto getting server/client hostname

--- client/src/api-helper/config.js
+++ client/src/api-helper/config.js
@@ -3,7 +3,8 @@
  * not meant to be imported by client
  */
 
-export const server_add = process.env.SERVER_ADD || "http://localhost:5000";
+export const server_add = process.env.REACT_APP_SERVER_ADD || "http://localhost:5000";
+
 export const init_params_get = {
     method: 'get',
     headers: {
